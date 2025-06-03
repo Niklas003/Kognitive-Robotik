c33f10a5e2a36fe2c58fb315839594a9a4580ad6
GrafBunsen <online@eisermann.xyz>
2025-06-03 15:12:04 +0200

    added basic touch sensor

--- controllers/touch_controller/controllers/touch_controller/touch_controller.py
+++ controllers/touch_controller/controllers/touch_controller/touch_controller.py
@@ -1,6 +1,7 @@
 """touch_controller controller."""
 
 from controller import Robot
+import csv
 
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
@@ -8,6 +9,7 @@ timestep = int(robot.getBasicTimeStep())
 joint_names = [
     "base","forearm","rotational_wrist","upperarm","wrist"
 ]
+joints = [robot.getDevice(name) for name in joint_names]
 
 #base: 0 - 6.03
 #forearm: 0 - 4.21
@@ -15,21 +17,25 @@ joint_names = [
 #upperarm: -2.44 - 0
 #wrist: -4.05 - 0
 
-joints = [robot.getDevice(name) for name in joint_names]
-
-touch1 = robot.getDevice("ts0")
-touch1.enable(timestep)
+touch_names = [
+    "ts0","ts1","ts2","ts3"
+]
+touchs = [robot.getDevice(name) for name in touch_names]
+for touch in touchs:
+    touch.enable(timestep)
 
 for joint in joints:
     joint.setPosition(0.0)
     joint.setVelocity(1.0)
 
-target_positions = [1, 1, -1, -1, -1]
+target_positions = [0, 2, 0, 0, 0]
 
 for i in range(len(joints)):
     joints[i].setPosition(target_positions[i])
 
 time = 0
 
-while robot.step(timestep) != -1: 
-    print(touch1.getValue())
+while robot.step(timestep) != -1:
+    if time % 100 == 0: 
+        for i in range(len(touchs)):
+            print(f"{touch_names[i]}: {touchs[i].getValue()/10}")
